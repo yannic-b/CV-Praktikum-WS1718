95bf03c4a80b1c585cfcf20351c107c24d9d390b
yannic-b <5boysen@informatik.uni-hamburg.de>
2018-01-10 12:50:37 +0100

    Please work, push

--- Projekt/Testing.py
+++ Projekt/Testing.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 """
 
 
@@ -84,4 +83,4 @@ model.fit_generator(
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
 
-model.save_weights('first_try.h5')
+# model.save_weights('first_try.h5')
