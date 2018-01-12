b12bd143ed73a1da419704b2f1ebec41272d093c
yannic-b <5boysen@informatik.uni-hamburg.de>
2018-01-12 20:45:31 +0100

    fix

--- Projekt/Testing.py
+++ Projekt/Testing.py
@@ -21,7 +21,7 @@ img_width, img_height = 300, 180
 train_data_dir = 'data/train'
 validation_data_dir = 'data/validation'
 
-nr_of_classes = len(os.listdir('data/validation'))
+nr_of_classes = len(os.listdir('data/validation')) - 1
 print nr_of_classes
 nb_train_samples = 32 * nr_of_classes
 nb_validation_samples = 18*nr_of_classes
